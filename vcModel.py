934ef23c2af7e532041d8f67bbf0c6dfee646845
vvas <vinedias@gmail.com>
2018-04-20 10:47:34 -0300

    todas as restrições de conservação de fluxo adicionadas

--- vcModel.py
+++ vcModel.py
@@ -31,33 +31,50 @@ class vcModel:
   def criterio1(self, l, x, vcm, L):
     """x = {(i,j): vcm.continuous_var(name='x_{0}_{1}'.format(i,j)) for i in range(1, 6) for j in range(1, 6)}"""
     x = {(0, i): vcm.continuous_var(name = 'x_{0}_{1}'.format(0, i)) for i in l}
+    x = {}
     for u in range(1, len(l), 1):
-      for i in range(1, L, 1): 
-        for j in range(1, L, 1):
+      for i in range(0, L+1, 1): 
+        for j in range(0, L+1, 1):
           for k in range(1, u, 1):
-            if ((0<i) & (i<j) & (j<=L) & (j-i == l[u])):
+            if ((0 < i) & (i < j) & (j <= L) & (j-i == l[u])):
               x = {(i, j): vcm.continuous_var(name = 'x_{0}_{1}'.format(i, j))}
-    #vcm.print_information()
+    vcm.print_information()
 
   def criterio2(self, L, lmin, x, vcm):
     x = {(i, i+1): vcm.continuous_var(name = 'x_{0}_{1}'.format(i, i+1)) for i in range(lmin, L-1, 1)}
     #print(x)
 
-  def conservF(self, vcm, p, q):
-    vcm.minimize(vcm.continuous_var())
+  def conservF(self, vcm, p, q, L, f, r1, r2, D):
+    vcm.minimize(vcm.integer_var())
     #tm.add_constraint(tm.sum(x[i,j] for j in target) <= capacities[i])
     j = vcm.number_of_continuous_variables
     for i in range(0, j-1):
       if (vcm.get_var_by_name('x_' + str(0) + '_' + str(i))):
         p.append(vcm.get_var_by_name('x_' + str(0) + '_' + str(i)))
+      else if (vcm.get_var_by_name('x_' + str(i) + '_' + str(L))):
+        q.append(vcm.get_var_by_name('x_' + str(i) + '_' + str(L)))
+    vcm.add_constraint(vcm.sum(p) == vcm.integer_var())
+    vcm.add_constraint(vcm.sum(q) == vcm.integer_var())
+    for h in range(1, L):
+      for j in range(0, L+1):
+        for g in range(0, L+1):
+          if (vcm.get_var_by_name('x_' + str(j) + '_' + str(h)) & vcm.get_var_by_name('x_' + str(h) + '_' + str(g))):
+            r1.append(vcm.get_var_by_name('x_' + str(j) + '_' + str(h)))
+            r2.append(vcm.get_var_by_name('x_' + str(h) + '_' + str(g)))
+    vcm.add_constraint(vcm.sum(r1) - vcm.sum(r2) == 0)
+    
+
+
     #vcm.add_constraint(vcm.continuous_var() <= )
     #vcm.get_var_by_index()
-    print(p)
+    #print(p)
+    #print(q)
 
   def getvar(self, vcm, y):
     j = vcm.number_of_continuous_variables
-    for i in range(0, j-1):
+    for i in range(0, j):
       y.append(vcm.get_var_by_index(i))
+
     #print(y)
   
   def method(self):
@@ -68,15 +85,19 @@ class vcModel:
   def __init__(self):
     print("iniciovalerio")
     l = [4, 3, 2]
+    D = [10, 5, 3]
     x = {}
-    y = []
+    y = [] #variavel auxiliar para imprimir a construção de arcos
     p = [] #variável auxiliar para construção das restrições
     q = [] #variável auxiliar para construção das restrições
+    r1 = [] #variável auxiliar para construção das restrições
+    r2 = [] #variável auxiliar para construção das restrições
     L = 9
+    f = []
     vcm = Model(name='valeriodecarvalho')
     lmin = np.amin(l)
     #self.method()
     self.criterio1(l, x, vcm, L)
     self.criterio2(L, lmin, x, vcm)
     self.getvar(vcm, y)
-    self.conservF(vcm, p, q)
+    self.conservF(vcm, p, q, L, f, r1, r2, D)
