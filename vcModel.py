1419e59126a0c6fbc4b00dbe64758a0de0a3908b
Vinicius Dias <vinicius.dias@fc.unesp.br>
2018-10-14 19:45:55 -0300

    Alterações para k objetos em estoque

--- vcModel.py
+++ vcModel.py
@@ -16,36 +16,43 @@ class vcModel:
   def criterio1(self, l, x, vcm, L):
     x = {(0, i): vcm.continuous_var(name = 'x_{0}_{1}'.format(0, i)) for i in l}
     x = {}
-    for u in range(0, len(l), 1):
-      for i in range(0, L+1, 1): 
-        for j in range(i, L+1, 1):
-          for k in range(0, u+1, 1):
-            if ((0 < i) & (i < j) & (j <= L) & (j-i == l[u]) & (self.inarc(i - l[k], i, vcm))): #problemas
-              if (not self.inarc(i, j, vcm)):
-                x = {(i, j): vcm.continuous_var(name = 'x_{0}_{1}'.format(i, j))}
+    for p in range(0, len(L), 1):
+      for u in range(0, len(l), 1):
+        for i in range(0, L[p]+1, 1): 
+          for j in range(i, L[p]+1, 1):
+            for k in range(0, u+1, 1):
+              if ((0 < i) & (i < j) & (j <= L[p]) & (j-i == l[u]) & (self.inarc(i - l[k], i, vcm))): #problemas
+                if (not self.inarc(i, j, vcm)):
+                  x = {(i, j): vcm.continuous_var(name = 'x_{0}_{1}'.format(i, j))}
                
   def criterio2(self, L, lmin, x, vcm):
-    x = {(i, i+1): vcm.continuous_var(name = 'x_{0}_{1}'.format(i, i+1)) for i in range(lmin, L, 1)}
+    x = {(i, i+1): vcm.continuous_var(name = 'x_{0}_{1}'.format(i, i+1)) for i in range(lmin, L[0], 1)}
     
 
   def conservF(self, vcm, p, q, L, l, f, r1, r2, D, d, ek):
     #vcm.continuous_var(name='z')
-    vcm.integer_var(name='z')
-    vcm.set_objective('min', vcm.get_var_by_name('z'))
+    z = []
+    {(k): vcm.continuous_var(name = 'z_{0}'.format(k)) for k in range(0, len(L), 1)}
+    for k in range(0, len(L)):
+      z.append(vcm.get_var_by_name('z_' + str(k)))
+    vcm.set_objective('min', vcm.sum(z))
     #restrições de conservação de fluxo
     j = vcm.number_of_continuous_variables
     for i in range(0, j-1):
       if (vcm.get_var_by_name('x_' + str(0) + '_' + str(i))):
         p.append(vcm.get_var_by_name('x_' + str(0) + '_' + str(i)))
-      if (vcm.get_var_by_name('x_' + str(i) + '_' + str(L))):
-        q.append(vcm.get_var_by_name('x_' + str(i) + '_' + str(L)))
-    vcm.add_constraint(vcm.get_var_by_name('z') == vcm.sum(p))
-    vcm.add_constraint(- vcm.get_var_by_name('z') == - vcm.sum(q))
-    for j in range(1, L, 1):
+    vcm.add_constraint(vcm.sum(z) == vcm.sum(p))
+    for k in range(0, len(L)):
+      q = []
+      for i in range(0, j-1):
+        if (vcm.get_var_by_name('x_' + str(i) + '_' + str(L[k]))):
+          q.append(vcm.get_var_by_name('x_' + str(i) + '_' + str(L[k])))
+      vcm.add_constraint(- vcm.get_var_by_name('z_' + str(k)) == - vcm.sum(q))
+    for j in range(1, L[0], 1):
       for i in range(0, j, 1):
         if (vcm.get_var_by_name('x_' + str(i) + '_' + str(j))):
           r1.append(vcm.get_var_by_name('x_' + str(i) + '_' + str(j)))
-      for k in range(j, L+1, 1):
+      for k in range(j, L[0]+1, 1):
         if (vcm.get_var_by_name('x_' + str(j) + '_' + str(k))):
           r2.append(vcm.get_var_by_name('x_' + str(j) + '_' + str(k)))
       if (bool(r1) & bool(r2)):
@@ -54,7 +61,7 @@ class vcModel:
       r2 = []  
     for i in range(len(l)):
       d = []
-      for k in range(0, L+1):
+      for k in range(0, L[0]+1):
         if (vcm.get_var_by_name('x_' + str(k) + '_' + str(k+l[i]))):
           d.append(vcm.get_var_by_name('x_' + str(k) + '_' + str(k+l[i])))
 
